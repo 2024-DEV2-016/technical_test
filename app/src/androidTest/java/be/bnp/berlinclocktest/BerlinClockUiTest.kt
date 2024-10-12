128002538387e6c0430a0bfa7cce4883b6f1312a
ArthurCowboy <51749833+ArthurCowboy@users.noreply.github.com>
2024-10-12 17:52:20 +0200

    Setup Light rows

--- app/src/androidTest/java/be/bnp/berlinclocktest/BerlinClockUiTest.kt
+++ app/src/androidTest/java/be/bnp/berlinclocktest/BerlinClockUiTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.onNodeWithTag
 import be.bnp.berlinclocktest.ui.theme.BerlinClockTestTheme
 import be.bnp.business.models.LightState
 import be.bnp.business.usecases.GetSecondsLightStateUseCase
+import be.bnp.business.usecases.GetUpperHoursLightStatesUseCase
 import org.junit.Rule
 import org.junit.Test
 
@@ -35,5 +36,16 @@ class BerlinClockUiTest {
 			}
 		}
 		composeTestRule.onNodeWithTag("LightBox${LightState.OFF}").assertExists()
+	}
+
+	@Test
+	fun lightRowLightsUpTheCorrectNumberOfLightsForUpperHours() {
+		val upperHoursLightStatesUseCase = GetUpperHoursLightStatesUseCase()
+		val lightStates = upperHoursLightStatesUseCase(13)
+		composeTestRule.setContent {
+			BerlinClockTestTheme {
+				LightRow(lightStates)
+			}
+		}
 	}
 }
---
+++ app/src/main/java/be/bnp/berlinclocktest/LightRow.kt
@@ -0,0 +1,8 @@
+package be.bnp.berlinclocktest
+
+import androidx.compose.runtime.Composable
+import be.bnp.business.models.LightState
+
+@Composable
+fun LightRow(lightStates: List<LightState>) {
+}
