ae0ad68ab380b1d536078e02964d7d1d15a2e8a1
ArthurCowboy <51749833+ArthurCowboy@users.noreply.github.com>
2024-10-12 17:55:48 +0200

    Test Light Row implementation

--- app/src/androidTest/java/be/bnp/berlinclocktest/BerlinClockUiTest.kt
+++ app/src/androidTest/java/be/bnp/berlinclocktest/BerlinClockUiTest.kt
@@ -1,11 +1,13 @@
 package be.bnp.berlinclocktest
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import be.bnp.berlinclocktest.ui.theme.BerlinClockTestTheme
 import be.bnp.business.models.LightState
 import be.bnp.business.usecases.GetSecondsLightStateUseCase
 import be.bnp.business.usecases.GetUpperHoursLightStatesUseCase
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -47,5 +49,13 @@ class BerlinClockUiTest {
 				LightRow(lightStates)
 			}
 		}
+
+		val offLightsCount = lightStates.filter { it == LightState.OFF }.size
+		val redLightsCount = lightStates.filter { it == LightState.RED }.size
+		val yellowLightsCount = lightStates.filter { it == LightState.YELLOW }.size
+
+		assertEquals(offLightsCount, composeTestRule.onAllNodesWithTag("LightBox${LightState.OFF}").fetchSemanticsNodes().size)
+		assertEquals(redLightsCount, composeTestRule.onAllNodesWithTag("LightBox${LightState.RED}").fetchSemanticsNodes().size)
+		assertEquals(yellowLightsCount, composeTestRule.onAllNodesWithTag("LightBox${LightState.YELLOW}").fetchSemanticsNodes().size)
 	}
 }
--- app/src/main/java/be/bnp/berlinclocktest/LightRow.kt
+++ app/src/main/java/be/bnp/berlinclocktest/LightRow.kt
@@ -1,8 +1,25 @@
 package be.bnp.berlinclocktest
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import be.bnp.business.models.LightState
 
 @Composable
 fun LightRow(lightStates: List<LightState>) {
+	Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+		lightStates.forEach { lightState ->
+			Box(modifier = Modifier
+				.height(50.dp)
+				.testTag("LightBox$lightState")
+				.background(getColorFromLightState(lightState)))
+		}
+	}
 }
